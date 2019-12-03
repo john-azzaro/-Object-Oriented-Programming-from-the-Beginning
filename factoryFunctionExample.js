8ebd9764ce63fad38dab71aeb8008348972632b0
John Azzaro <jazzaro@temple.edu>
2019-12-02 22:12:23 -0800

    updated facotry function example

--- factoryFunctionExample.js
+++ factoryFunctionExample.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// factory function
 function createCar(make, model, year) {
   return {
     make: make,
@@ -12,13 +13,10 @@ function createCar(make, model, year) {
   };
 }
 
+// create a new object using factory function
 const makeFerrari = createCar('Ferrari', '458 spider', 2015);
 const makePorsche = createCar('Porsche', '718 GT4', 2019);
 
-
-
-
+// call new object
 console.log(makeFerrari);
-
-
 console.log(makeFerrari.startIgnition());
