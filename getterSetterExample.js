0821546da0e9cd47458745eb71659dc75bacedcd
John Azzaro <jazzaro@temple.edu>
2019-12-05 20:56:03 -0800

    updated getter/setter example

--- getterSetterExample.js
+++ getterSetterExample.js
@@ -12,14 +12,31 @@ function Car(make, model) {
     console.log("vrooom vrooom");
   };
   
-  Object.defineProperties(this, 'isAutomobile', {   // first argument in the method is the object you want a new property to (i.e. this), second is the name of the property, third is an object with the key of "get" and the value iss a function.
-    get: function() {                               // and when you call the coupe.isAutomobile, it will return whatever is in this function... in this case, it returns isAutomobile
-      return isAutomobile;
+                                                 // Object.defineProperty is used to define getters or setters
+  Object.defineProperty(this, 'isAutomobile', {   // first argument in the method is the object you want a new property to (i.e. this), second is the name of the READ-ONLY property, third is an object with the key of "get" and the value iss a function.
+    get: function() {                               // and when you call the coupe.isAutomobile, it will return whatever is in this function... in this case, it returns isAutomobile.
+      return isAutomobile;                          // "isAutomobile" is part of the closure of the function
+    },
+    set: function(value) {                          // if you want to set the value of this function from outside the object, 
+      isAutomobile = value;
     }
   });            
 }
 
 const coupe = new Car("porsche", 911);
 
-// console.log(coupe.isAutomobile());
+
+console.log(coupe.isAutomobile);
+
+coupe.isAutomobile = false;
+
+console.log(coupe.isAutomobile);
+
+
+
+
+
+
+
+
 
