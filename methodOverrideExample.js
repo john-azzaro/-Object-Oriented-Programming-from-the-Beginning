64d278411ae189029217b296ed55181811c971e4
John Azzaro <jazzaro@temple.edu>
2019-12-11 21:11:43 -0800

    updated

---
+++ methodOverrideExample.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function extend(Child, Parent) {                           // extend function
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
+function Automobile() {                                     // constructor
+}
+
+Automobile.prototype.ignition = function() {                // method on Automobile prototype
+  console.log('vrooom')
+}
