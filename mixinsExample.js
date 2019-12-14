9d6c7d3ba5119db6b61c56783d36a6ddb24ec8d4
John Azzaro <jazzaro@temple.edu>
2019-12-13 20:33:08 -0800

    added mixin example, additional pn

--- mixinsExample.js
+++ mixinsExample.js
@@ -1,1 +1,26 @@
 "use strict";
+
+function mixin(target, ...sources) {  
+  Object.assign(target, ...sources);
+}
+
+const isWorking = {
+  engineRuns: function() {
+    console.log('engine is running');
+  }
+}
+
+const isRegistered = {
+  paperWork: function() {
+    console.log('paperwork checks out!')
+  }
+}
+
+function Vehicle() {   
+}
+
+mixin(Vehicle.prototype, isWorking, isRegistered);  
+
+const porsche = new Vehicle();    
+
+console.log(porsche);    
